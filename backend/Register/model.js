73c89fcddfafbeef05ab4d590400d4dcd91fb1cc
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-03-28 18:47:53 +0530

    Add the passportjs for hashing and salting with passport-local and local-mongoose just with pass the value and userName should have unique value

--- backend/Register/model.js
+++ backend/Register/model.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 // const { Schema } = require("../Tool/model");
+const passportLocalMongoose=require("passport-local-mongoose");
 const {Schema}=mongoose;
 const Signup = new mongoose.Schema({
   yourName: String,
   Email: String,
-  userName: String,
+  userName: {
+    type: String,
+    unique: true
+  },
   PassWord: String,
 });
+
+signupListing.plugin(passportLocalMongoose);
 let signupListing = mongoose.model("signupListing", Signup);
 module.exports = signupListing;
