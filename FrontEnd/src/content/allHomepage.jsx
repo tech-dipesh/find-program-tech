3a8d47da905c4d02122257d0ee254dc190d6268f
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-04-07 11:30:35 +0530

    add the loading state

--- FrontEnd/src/content/allHomepage.jsx
+++ FrontEnd/src/content/allHomepage.jsx
@@ -4,7 +4,20 @@ import Hero from "./firstpage.jsx";
 import Testimonial from "./testimonial.jsx";
 import Footer from "../Layout/footer.jsx";
 import MainNavbar from "../Layout/mainNavbar.jsx";
+import Loading from "../Miscellaneous/Loading.jsx";
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [loading, setloading] = useState(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setloading(false)
+    }, 100);
+  }, [])
+  
+  if(loading){
+    return <Loading/>
+  }
+
   return (
     <>
       <div className="justify-center">
